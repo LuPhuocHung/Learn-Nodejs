5ac7afd51966b1d372947a3df0f802100dd197ac
Lu Phuong <hauhunghuy1998@gmail.com>
2022-07-10 23:08:18 +0700

    hay

--- src/index.js
+++ src/index.js
@@ -2,31 +2,35 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const route = require('./routes');
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+); // gửi data từ form  ==> req.body
+app.use(express.json()); // gửi từ code js ==> req.body
 // HTTP logger
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 // template engine
 //app.engine('handlebars', handlebars.engine());
-app.engine('hbs', handlebars.engine({ 
-  extname: '.hbs'
- }));
+app.engine(
+  'hbs',
+  handlebars.engine({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// routes init
+    route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
-})
+});
